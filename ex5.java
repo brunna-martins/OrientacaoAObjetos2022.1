69487738a52a2a9be50274a1a8df707debbf2bf4
Brunna Louise <brunna.louisemr@gmail.com>
2022-06-14 13:35:46 -0300

    alterations

--- ex5.java
+++ ex5.java
@@ -20,20 +20,79 @@ public class ex5 {
 		vetorInteiros = new int[x]; // criação do vetor
 		for (int i=0;i<x;++i) {
 			vetorInteiros[i] = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor a ser armazenado na posição "+i+" do vetor."));
-			JOptionPane.showMessageDialog(null, vetorInteiros[i]);
 		}
-		float mediaVetor = media(vetorInteiros[],x);
-		// JOptionPane.showMessageDialog(null, "A média dos valores armazenados no vetor é "+media(vetorInteiros[],x));
-		
+		double mediaVetor = media(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A média dos valores armazenados no vetor é "+mediaVetor+".");
+		int quantidadePositivos = positivo(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A quantidade de valores positivos armazenados no vetor é "+quantidadePositivos+".");
+		int quantidadeNegativos = negativo(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A quantidade de valores negativos armazenados no vetor é "+quantidadeNegativos+".");
+		int quantidadeZeros = zero(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A quantidade de valores nulos armazenados no vetor é "+quantidadeZeros+".");
+		int quantidadePares = par(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A quantidade de valores pares armazenados no vetor é "+quantidadePares+".");
+		int quantidadeImpares = impar(vetorInteiros,x);
+		JOptionPane.showMessageDialog(null, "A quantidade de valores ímpares armazenados no vetor é "+quantidadeImpares+".");
 	}
 
-	public static float media(int vetor[],int tamanhoVetor) {
-		int soma = 0;
+	public static double media(int vetor[],int tamanhoVetor) {
+		double soma = 0;
 		
 		for (int i=0;i<tamanhoVetor;++i) {
 			soma = soma + vetor[i];
 		}
-		float mediaCalc = (float) (soma/(tamanhoVetor));
+		double mediaCalc = (soma/(tamanhoVetor));
 		return mediaCalc;
 	}
+	
+	public static int positivo(int vetor[],int tamanhoVetor) {
+		int positivos = 0;
+		for (int i=0;i<tamanhoVetor;++i) {
+			if (vetor[i]>0) {
+				positivos = positivos + 1;
+			}
+		}
+		return positivos;
+	}
+	
+	public static int negativo(int vetor[],int tamanhoVetor) {
+		int negativos = 0;
+		for (int i=0;i<tamanhoVetor;++i) {
+			if (vetor[i]<0) {
+				negativos = negativos + 1;
+			}
+		}
+		return negativos;
+	}
+	
+	public static int zero(int vetor[],int tamanhoVetor) {
+		int zeros = 0;
+		for (int i=0;i<tamanhoVetor;++i) {
+			if (vetor[i]==0) {
+				zeros = zeros + 1;
+			}
+		}
+		return zeros;
+	}
+	
+	public static int par(int vetor[],int tamanhoVetor) {
+		int pares = 0;
+		for (int i=0;i<tamanhoVetor;++i) {
+			if (vetor[i]%2==0) {
+				pares = pares + 1;
+			}
+		}
+		return pares;
+	}
+	
+	public static int impar(int vetor[],int tamanhoVetor) {
+		int impares = 0;
+		for (int i=0;i<tamanhoVetor;++i) {
+			if (vetor[i]%2!=0) {
+				impares = impares + 1;
+			}
+		}
+		return impares;
+	}
+	
 }
